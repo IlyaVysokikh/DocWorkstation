4b8252a1dfdb29a19797f257f05a80ea10522843
vysokikh_ia <vysokikh_ia@taximaxim.ru>
2025-03-16 23:00:13 +0500

    initial commit

---
+++ src/main/java/org/example/docworkstation/DocWorkstationApplication.java
@@ -0,0 +1,13 @@
+package org.example.docworkstation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocWorkstationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DocWorkstationApplication.class, args);
+	}
+
+}
